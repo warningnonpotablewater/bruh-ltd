9c04bec9d046a97d3edc2ada37c1257a066ab090
kirbykevinson <kirbykevinson@outlook.com>
2021-03-12 16:24:26 +0700

    Add a copyright notice script

---
+++ public/licensing.js
@@ -0,0 +1,25 @@
+const container = document.createElement("div");
+
+container.innerHTML = "© 2021 Bruh, Ltd. ";
+
+container.style.position = "fixed";
+container.style.bottom = "0px";
+container.style.right = "0px";
+container.style.fontSize = "12pt";
+container.style.fontFamily = "sans-serif";
+container.style.color = "#FFFFFF";
+container.style.background = "#0000007F";
+
+const link = document.createElement("a");
+
+link.innerHTML = "Learn more";
+
+link.href = "https://gitlab.com/kirbykevinson/bruh.ltd/-/blob/master/LICENSE_LIST.md";
+
+link.style.textDecoration = "underline";
+link.style.color = "#FBAAC0";
+
+container.appendChild(link);
+container.innerHTML += ".";
+
+document.body.appendChild(container);
