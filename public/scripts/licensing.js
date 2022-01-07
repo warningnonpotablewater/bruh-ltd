8f5ca51e8dfa01e65e893237243c0509b7643248
kirbykevinson <kirbykevinson@outlook.com>
2022-01-07 18:48:23 +0700

    Move the licensing styles to CSS

--- public/scripts/licensing.js
+++ public/scripts/licensing.js
@@ -2,23 +2,13 @@ const container = document.createElement("div");
 
 container.innerHTML = "© 2021 Bruh, Ltd. ";
 
-container.style.all = "initial";
-container.style.position = "fixed";
-container.style.bottom = "0px";
-container.style.right = "0px";
-container.style.fontSize = "12pt";
-container.style.fontFamily = "sans-serif";
-container.style.color = "#FFFFFF";
-container.style.background = "#0000007F";
+container.className = "licensing";
 
 const link = document.createElement("a");
 
 link.innerHTML = "Learn more";
 
 link.href = "https://gitlab.com/kirbykevinson/bruh-ltd/-/blob/master/LICENSE_LIST.md";
-
-link.style.textDecoration = "underline";
-link.style.color = "#FBAAC0";
 
 container.appendChild(link);
 container.innerHTML += ".";
